91bc9bdc30c127d6ed62e44de9fe91046042ca06
Aryan Agarwal <aryankush025@gmail.com>
2024-12-12 19:21:28 +0530

    Hash Table Implementation

--- hash-table.js
+++ hash-table.js
@@ -10,10 +10,47 @@ class HashTable {
     }
     return hash;
   }
+
+  set(key, value) {
+    const address = this._hash(key);
+
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+  }
+
+  get(key) {
+    const address = this._hash(key);
+
+    if (!this.data[address]) {
+      return undefined;
+    }
+
+    const [, value] = this.data[address].find(([k, v]) => k === key);
+
+    return value;
+  }
+
+  keys() {
+    const keys = [];
+
+    this.data.forEach((data) => {
+      if (data && Array.isArray(data)) {
+        data.forEach(([key, value]) => {
+          keys.push(key);
+        });
+      }
+    });
+
+    return keys;
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set("grapes", 10000);
-myHashTable.get("grapes");
+console.log(myHashTable.get("grapes"));
 myHashTable.set("apples", 9);
-myHashTable.get("apples");
+console.log(myHashTable.get("apples"));
+console.log(myHashTable.keys());
