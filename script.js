80b3230e1ba7b3d13f9c3d7d87384f2d8fa170b1
Aryan Agarwal <aryankush025@gmail.com>
2024-12-28 01:08:21 +0530

    Comments out Promise.all implementation example
    Temporarily disables custom Promise.all implementation and comparison test
    to focus on core functionality. Keeps sum function active while
    Promise-related code is under review.

--- script.js
+++ script.js
@@ -1,72 +1,72 @@
-const p1 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("Promise 1 resolved");
-  }, 3000);
-});
+// const p1 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("Promise 1 resolved");
+//   }, 3000);
+// });
 
-const p2 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("Promise 2 resolved");
-  }, 1000);
-});
+// const p2 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("Promise 2 resolved");
+//   }, 1000);
+// });
 
-const p3 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve("Promise 3 resolved");
-  }, 3000);
-});
+// const p3 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve("Promise 3 resolved");
+//   }, 3000);
+// });
 
-function promiseAll(promises) {
-  return new Promise((resolve, reject) => {
-    const results = Array(promises.length).fill(0);
-    let count = 0;
+// function promiseAll(promises) {
+//   return new Promise((resolve, reject) => {
+//     const results = Array(promises.length).fill(0);
+//     let count = 0;
 
-    promises.forEach((promise, index) => {
-      promise
-        .then((response) => {
-          results[index] = response;
-          count++;
+//     promises.forEach((promise, index) => {
+//       promise
+//         .then((response) => {
+//           results[index] = response;
+//           count++;
 
-          if (count === promises.length) {
-            resolve(results);
-          }
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  });
-}
+//           if (count === promises.length) {
+//             resolve(results);
+//           }
+//         })
+//         .catch((error) => {
+//           reject(error);
+//         });
+//     });
+//   });
+// }
 
-Promise.promiseAll = promiseAll;
+// Promise.promiseAll = promiseAll;
 
-console.log(Promise.promiseAll);
+// console.log(Promise.promiseAll);
 
-console.time("Time");
+// console.time("Time");
 
-Promise.promiseAll([p1, p2, p3])
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.timeEnd("Time");
-  });
+// Promise.promiseAll([p1, p2, p3])
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.timeEnd("Time");
+//   });
 
-console.time("Time2");
+// console.time("Time2");
 
-Promise.all([p1, p2, p3])
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.timeEnd("Time2");
-  });
+// Promise.all([p1, p2, p3])
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.timeEnd("Time2");
+//   });
 
 const sum = (a, b) => {
   return a + b;
