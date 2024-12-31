1629263929591ed61b72e29ca0ec081ebc69a62b
Aryan Agarwal <aryankush025@gmail.com>
2024-12-31 22:06:53 +0530

    Replaces merge sort with array flatten implementation
    Removes previous mergeSortedArrays implementation and adds a new flat function
    that handles array flattening with configurable depth. The implementation
    properly handles sparse arrays and nested array structures.
    
    Updates HTML to reference arrays.js instead of curry.js for active development.

--- arrays.js
+++ arrays.js
@@ -18,40 +18,82 @@
 
 // Merge sorted array
 
-const mergeSortedArrays = (arr1, arr2) => {
-  if (
-    !(arr1 && typeof arr1.length === "number") ||
-    !(arr2 && typeof arr2.length === "number")
-  ) {
-    return "Oops! please send a proper array with values.";
-  }
+// const mergeSortedArrays = (arr1, arr2) => {
+//   if (
+//     !(arr1 && typeof arr1.length === "number") ||
+//     !(arr2 && typeof arr2.length === "number")
+//   ) {
+//     return "Oops! please send a proper array with values.";
+//   }
 
-  if (arr1.length === 0) {
-    return arr2;
-  }
+//   if (arr1.length === 0) {
+//     return arr2;
+//   }
 
-  if (arr2.length === 0) {
-    return arr1;
-  }
+//   if (arr2.length === 0) {
+//     return arr1;
+//   }
 
-  const mergedArray = [];
-  let i = 0;
-  let j = 0;
+//   const mergedArray = [];
+//   let i = 0;
+//   let j = 0;
 
-  while (i < arr1.length || j < arr2.length) {
-    console.log(arr1[i], arr2[j]);
-    console.log("mergedArray", mergedArray);
+//   while (i < arr1.length || j < arr2.length) {
+//     console.log(arr1[i], arr2[j]);
+//     console.log("mergedArray", mergedArray);
 
-    if (arr2[j] === undefined || arr1[i] < arr2[j]) {
-      mergedArray.push(arr1[i]);
-      i++;
-    } else {
-      mergedArray.push(arr2[j]);
-      j++;
+//     if (arr2[j] === undefined || arr1[i] < arr2[j]) {
+//       mergedArray.push(arr1[i]);
+//       i++;
+//     } else {
+//       mergedArray.push(arr2[j]);
+//       j++;
+//     }
+//   }
+
+//   return mergedArray;
+// };
+
+// console.log(mergeSortedArrays([1, 10, 99], [-111, 0]));
+
+const flat = function (arr, depth = 1) {
+  console.log(arr);
+
+  for (let k = 0; k < depth; k++) {
+    let newArr = [];
+
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+      const el = arr[i];
+
+      if (i in arr) {
+        if (Array.isArray(el)) {
+          el.forEach((element, idx) => {
+            if (idx in el) {
+              newArr.push(element);
+            }
+          });
+        } else {
+          count++;
+          newArr.push(el);
+        }
+      }
     }
+
+    if (count === arr.length) {
+      break;
+    }
+
+    arr = newArr;
   }
 
-  return mergedArray;
+  return arr;
 };
 
-console.log(mergeSortedArrays([1, 10, 99], [-111, 0]));
+const arr = [1, 2];
+arr[4] = undefined;
+arr[5] = [3, 4];
+arr[5][4] = [5, 6, [7, 8, [9, 10]]];
+
+console.log(flat(arr, Infinity)); // [1, 2, undefined, 3, 4, 5, 6, 7, 8, 9, 10]
